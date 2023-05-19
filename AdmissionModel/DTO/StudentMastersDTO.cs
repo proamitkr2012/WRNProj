27dc3b30cfe446127a2b7d7133c7a4cd9f9a3460
Amit kumar <92441167+proamitkr2012@users.noreply.github.com>
2023-05-19 15:21:26 +0530

    PUSH

--- AdmissionModel/DTO/StudentMastersDTO.cs
+++ AdmissionModel/DTO/StudentMastersDTO.cs
@@ -12,7 +12,7 @@ namespace AdmissionModel.DTO
     {
                 public long EntryID { get; set; }
         public string Roll { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public string? FatherName { get; set; }
         public string? MotherName { get; set; }
         public string? Email { get; set; }
